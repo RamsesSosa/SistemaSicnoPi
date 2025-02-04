b5f2d9ecf26325319046b447e0523003d4370525
RamsesSosa04 <sosaramses08@gmail.com>
2025-02-04 00:15:33 -0700

    Configuracion de enpoints de la api

--- api/views.py
+++ api/views.py
@@ -2,19 +2,37 @@ from rest_framework import viewsets
 from .serializer import *
 from .models import *
 
-# Create your views here.
+# ViewSet para cada modelo
 
-class usuarioViewSet(viewsets.ModelViewSet):
+class UsuarioViewSet(viewsets.ModelViewSet):
     queryset = Usuario.objects.all()
     serializer_class = UsuarioSerializer
 
-class clienteViewSet(viewsets.ModelViewSet):
-    queryset = Cliente.all()
+class ClienteViewSet(viewsets.ModelViewSet):
+    queryset = Cliente.objects.all()
     serializer_class = ClienteSerializer
 
-class equipoSerializer(viewsets.ModelViewSet):
-    queryset = Equipo.all()
+class EquipoViewSet(viewsets.ModelViewSet):
+    queryset = Equipo.objects.all()
     serializer_class = EquipoSerializer
 
-class 
+class EstadoCalibracionViewSet(viewsets.ModelViewSet):
+    queryset = EstadoCalibracion.objects.all()
+    serializer_class = EstadoCalibracionSerializer
+
+class HistorialEquipoViewSet(viewsets.ModelViewSet):
+    queryset = HistorialEquipo.objects.all()
+    serializer_class = HistorialEquipoSerializer
+
+class AlertaViewSet(viewsets.ModelViewSet):
+    queryset = Alerta.objects.all()
+    serializer_class = AlertaSerializer
+
+class ReporteViewSet(viewsets.ModelViewSet):
+    queryset = Reporte.objects.all()
+    serializer_class = ReporteSerializer
+
+class EntregaRecoleccionViewSet(viewsets.ModelViewSet):
+    queryset = EntregaRecoleccion.objects.all()
+    serializer_class = EntregaRecoleccionSerializer
     
