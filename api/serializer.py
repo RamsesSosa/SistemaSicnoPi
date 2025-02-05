8444ee106c650022e5247eb4f63c876c9546174a
RamsesSosa <ramses.villegas244689@potros.itson.edu.mx>
2025-02-04 22:53:00 -0700

    En vez de importar todo con *, importe cada uno

--- api/serializer.py
+++ api/serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import *
+from .models import Usuario, Cliente, Equipo, EstadoCalibracion, HistorialEquipo, Alerta, Reporte, EntregaRecoleccion
 
 class UsuarioSerializer(serializers.ModelSerializer):
     class Meta:
