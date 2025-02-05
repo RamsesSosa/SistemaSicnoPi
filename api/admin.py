c8ac6bd92de5b6121dff537a1847a9c8ed7463e6
RamsesSosa <ramses.villegas244689@potros.itson.edu.mx>
2025-02-04 22:55:56 -0700

    En vez de importar todo con *, importe cada uno desde models

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import *
+from .models import Usuario, Cliente, Equipo, EstadoCalibracion, HistorialEquipo, Alerta, Reporte, EntregaRecoleccion
 
 # Register your models here.
 
