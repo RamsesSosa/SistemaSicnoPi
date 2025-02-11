5e0e1bf06f141b54f2d7a19ed9b72378c76ab445
javier-Sc1 <francisco.soto244621@potros.itson.edu.mx>
2025-02-10 22:42:19 -0700

    formulario por terminar

--- frontweb/src/App.jsx
+++ frontweb/src/App.jsx
@@ -1,14 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
-import Registro from './components/Registro';
+import Register from './components/Register';
+import Home from './components/Home';
 
 const App = () => {
   return (
     <Router>
       <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/" element={<Login />} />
-        <Route path="/registro" element={<Registro />} />
-        <Route path="/dashboard" element={<h1>Dashboard</h1>} />
       </Routes>
     </Router>
   );
---
+++ frontweb/src/components/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div className="home-container">
+      <h1>Bienvenido a la Página de Inicio</h1>
+      <p>Esta es una página vacía después de iniciar sesión.</p>
+    </div>
+  );
+};
+
+export default Home;
