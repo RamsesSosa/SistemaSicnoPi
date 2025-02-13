41b8a154446ffa5afdbd39870f064ead748f84e9
RamsesSosa04 <sosaramses08@gmail.com>
2025-02-13 16:06:42 -0700

    Modfique servicios

--- FrontendMovil/services/api.js
+++ FrontendMovil/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.0.100:8000/api',  // Cambia esto por la URL de tu API
+  baseURL: 'http://192.168.0.100:8000/api',  
 });
 
 export const login = async (username, password) => {
